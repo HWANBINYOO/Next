6e7c31bed7117d62ad23bc246377fc72e40fb817
HAWNBINYOO <dngh0825@naver.com>
2022-04-27 16:15:38 +0900

    next api data 가져오기

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import Nhead from "../components/Nhead";
 
+const API_KEY = "8afa61e941198ab6f9244bdbfb66e9d3";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Nhead title="Home" />
-      <h1 className="active">Hello</h1>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
