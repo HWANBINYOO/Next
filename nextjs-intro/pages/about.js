b028026be866e6808ef7041494f32752d658307e
HAWNBINYOO <dngh0825@naver.com>
2022-04-27 14:33:39 +0900

    Layout Head 추가

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,6 +1,9 @@
+import Nhead from "../components/Nhead";
+
 export default function Prnguin() {
   return (
     <div>
+      <Nhead title="About" />
       <h1>😊</h1>
     </div>
   );
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,6 +1,9 @@
+import Nhead from "../components/Nhead";
+
 export default function Home() {
   return (
     <div>
+      <Nhead title="Home" />
       <h1 className="active">Hello</h1>
     </div>
   );
