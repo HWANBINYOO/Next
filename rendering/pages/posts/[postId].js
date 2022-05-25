fd63e6382c08ea2a7635d46a99945ad1b8fd8eb6
HAWNBINYOO <dngh0825@naver.com>
2022-05-25 21:06:09 +0900

    getStaticPaths fallback true

--- rendering/pages/posts/[postId].js
+++ rendering/pages/posts/[postId].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 function Post({ post }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <>
       <h2>
@@ -12,24 +20,23 @@ function Post({ post }) {
 export default Post;
 
 export async function getStaticPaths() {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts  `);
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   const data = await response.json();
 
-  const paths = data.map((psot) => {
-    return {
-      params: {
-        postId: `${post.id}`,
-      },
-    };
-  });
+  //   const paths = data.map((psot) => {
+  //     return {
+  //       params: {
+  //         postId: `${post.id}`,
+  //       },
+  //     };
+  //   });
   return {
-    // paths: [
-    //   { params: { postId: "1" } },
-    //   { params: { postId: "2" } },
-    //   { params: { postId: "3" } },
-    // ],
-    paths,
-    fallback: false,
+    paths: [
+      { params: { postId: "1" } },
+      { params: { postId: "2" } },
+      { params: { postId: "3" } },
+    ],
+    fallback: true,
   };
 }
 
@@ -39,6 +46,14 @@ export async function getStaticProps(context) {
     `https://jsonplaceholder.typicode.com/posts/${params.postId}`
   );
   const data = await response.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  console.log(`Generating page for /postts/${params.postId}`);
 
   return {
     props: {
