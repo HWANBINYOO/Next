8d6e5748eb88496561fd8b6d79fbb80137af1262
HAWNBINYOO <dngh0825@naver.com>
2022-06-25 11:06:25 +0900

    6/25

---
+++ portfolio/src/components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <>
+      <h1>해더입니다.</h1>
+    </>
+  );
+}
---
+++ portfolio/src/components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <>
+      <h1>해더입니다.</h1>
+    </>
+  );
+}
