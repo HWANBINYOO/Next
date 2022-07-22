ffc5d8e6ad3f660ca36ffc5378d676ef9ab9e9e2
hawnbin <dngh0825@naver.com>
2022-07-22 23:24:40 +0900

    왤케 어려움..

--- blog/utils/lib/CustomAxois.ts
+++ blog/utils/lib/CustomAxois.ts
@@ -1,27 +1,6 @@
 import axios from "axios";
 import Storage from "../Storage";
 
-// const CustomAxois = () => {
-//   try {
-//     const header = Object.assign({
-//       "Access-Control-Allow-Origin": "*",
-//       "Access-Control-Allow-Headers": "Content-Type,Authorization",
-//       "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-//       "Access-Control-Alllow-Credentials": true,
-//       "Content-Type": "application/json",
-//       "Cache-Control": "no-store",
-//       // Authorization: window.localStorage.getItem("Blog_accessToken") ?? "",
-//     });
-//     return axios.create({
-//       baseURL: "https://server.dev-log.kr",
-//       headers: header,
-//     });
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-// export default CustomAxois;
-
 const CustomAxois = axios.create({
   baseURL: "https://server.dev-log.kr/",
   headers: {
---
+++ blog/utils/lib/cookie.ts
@@ -0,0 +1,27 @@
+import CustomAxois from "./CustomAxois";
+import { GetServerSidePropsContext } from "next";
+
+const userCheck = async (ctx: GetServerSidePropsContext) => {
+  let accessToken = ctx.req.cookies["accessToken"];
+  const refreshToken = ctx.req.cookies["refreshToken"];
+  let cookies: string[] | undefined;
+
+  if (!accessToken) {
+    const res = await CustomAxois.post(
+      "/auth/refresh/web",
+      {},
+      { headers: { cookie: `refreshToken=${refreshToken};` } }
+    );
+
+    cookies = res.headers["set-cookie"];
+    accessToken = res.data.accessToken;
+  } else {
+    await CustomAxois.get("/auth/check", {
+      headers: { cookie: `accessToken=${accessToken};` },
+    });
+  }
+
+  return { cookies, accessToken };
+};
+
+export default userCheck;
