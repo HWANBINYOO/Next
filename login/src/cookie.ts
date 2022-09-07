2f90daa97abc8846850ac5183c17e7e3bcd716ed
hawnbin <dngh0825@naver.com>
2022-09-07 15:38:18 +0900

    login

---
+++ login/src/cookie.ts
@@ -0,0 +1,12 @@
+import { GetServerSidePropsContext } from "next";
+
+const Cookie = async (ctx: GetServerSidePropsContext) => {
+  let accessToken = ctx.req.cookies["Blog_accessToken"] ;
+  const refreshToken = ctx.req.cookies["Blog_refreshToken"];
+  let cookies: string[] | undefined;
+
+  
+  return { cookies, accessToken };
+};
+
+export default Cookie;
