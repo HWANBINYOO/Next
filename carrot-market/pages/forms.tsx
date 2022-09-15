e17efbe3638a54f3373e4657164e8981c776b573
hawnbin <dngh0825@naver.com>
2022-09-15 21:11:29 +0900

    useForm  handleSubmit

--- carrot-market/pages/forms.tsx
+++ carrot-market/pages/forms.tsx
@@ -9,14 +9,15 @@ import { useForm } from "react-hook-form";
 // Easier Inputs (c)
 
 export default function Forms(){
-   const {register , watch} = useForm();
-   console.log(watch());
-   
+    const { register , handleSubmit } = useForm();
+    const onValid = () => {
+        console.log("im valid bby");
+    };
     return (
-        <form >
-            <input {...register("username")} type="text" placeholder="Username" required />
-            <input {...register("email")} type="email" placeholder="Email" required />
-            <input {...register("password")} type="password" placeholder="password" required />
+        <form onSubmit={handleSubmit(onValid)}>
+            <input {...register("username" , {required:true, })} type="text" placeholder="Username"  />
+            <input {...register("email" , {required:true, })} type="email" placeholder="Email"  />
+            <input {...register("password" , {required:true, })} type="password" placeholder="password"  />
             <input type="submit" value="Create Account" />
         </form>
     )
