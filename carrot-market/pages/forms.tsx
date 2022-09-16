7721cc353f94034f819decb53c6f41286ae25d33
hawnbin <dngh0825@naver.com>
2022-09-16 20:35:44 +0900

    Validation part Two

--- carrot-market/pages/forms.tsx
+++ carrot-market/pages/forms.tsx
@@ -8,16 +8,28 @@ import { useForm } from "react-hook-form";
 // Dont deal with events (c)
 // Easier Inputs (c)
 
+interface LoginForm {
+    username: string;
+    password: string;
+    email:    string;
+}
+
 export default function Forms(){
-    const { register , handleSubmit } = useForm();
-    const onValid = () => {
+    const { register , handleSubmit } = useForm<LoginForm>();
+    const onValid = (data:LoginForm) => {
         console.log("im valid bby");
     };
+    const onInvalid = (errors : FieldErrors)  => {
+        console.log(errors);
+    };
     return (
-        <form onSubmit={handleSubmit(onValid)}>
-            <input {...register("username" , {required:true, })} type="text" placeholder="Username"  />
-            <input {...register("email" , {required:true, })} type="email" placeholder="Email"  />
-            <input {...register("password" , {required:true, })} type="password" placeholder="password"  />
+        <form onSubmit={handleSubmit(onValid , onInvalid)}>
+            <input {...register("username" , {required:"Username is required", minLength : {
+                message:"The username should be longer than 5 chars.",
+                value:5,
+            }, })} type="text" placeholder="Username"  />
+            <input {...register("email" , {required:"Email is required", })} type="email" placeholder="Email"  />
+            <input {...register("password" , {required:"Password is required", })} type="password" placeholder="password"  />
             <input type="submit" value="Create Account" />
         </form>
     )
