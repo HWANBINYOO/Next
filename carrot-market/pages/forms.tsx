c0e2ea28ccab3d7fea5e3188823c1075c46d1a2a
hawnbin <dngh0825@naver.com>
2022-09-18 02:22:44 +0900

    Error 처리

--- carrot-market/pages/forms.tsx
+++ carrot-market/pages/forms.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import {FieldErrors , useForm } from "react-hook-form";
 
 // Less code (c)
-// Better vlidation
+// Better vlidation (c)
 // Better validation( set , clear , display)
 // Habe control over inputs
 // Dont deal with events (c)
@@ -15,7 +15,9 @@ interface LoginForm {
 }
 
 export default function Forms(){
-    const { register , handleSubmit } = useForm<LoginForm>();
+    const { register , handleSubmit , formState: { errors } ,} = useForm<LoginForm>({
+        mode:"onChange"
+    });
     const onValid = (data:LoginForm) => {
         console.log("im valid bby");
     };
@@ -28,7 +30,20 @@ export default function Forms(){
                 message:"The username should be longer than 5 chars.",
                 value:5,
             }, })} type="text" placeholder="Username"  />
-            <input {...register("email" , {required:"Email is required", })} type="email" placeholder="Email"  />
+
+            <input
+            {...register("email" , {
+                required:"Email is required",
+                validate:{ 
+                    notGmail: (value) =>
+                      !value.includes("@gmail.com") || "Gmail is not allowed",
+                    },
+                })} 
+                type="email"
+                placeholder="Email"
+                // className={`${Boolean(errors.email) ? "border-red-500" : ""}`}
+            />
+            {errors.email?.message}
             <input {...register("password" , {required:"Password is required", })} type="password" placeholder="password"  />
             <input type="submit" value="Create Account" />
         </form>
