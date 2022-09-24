f3666f2c85e605174f2b4ccc2c50fff0cc5a5c27
hawnbin <dngh0825@naver.com>
2022-09-24 20:05:46 +0900

    fix Serverless Sessions

--- carrot-market/libs/server/withHandler.ts
+++ carrot-market/libs/server/withHandler.ts
@@ -1,23 +1,26 @@
-import { NextApiHandler , NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export interface ResponseType {
-    ok: boolean;
-    [key: string]:any;
+  ok: boolean;
+  [key: string]: any;
 }
 
 export default function withHandler(
-    method: "GET" | "POST" | "DELETE" ,
-    fn: NextApiHandler,
-    ): NextApiHandler {
-    return async function(req:NextApiRequest, res:NextApiResponse){
-        if(req.method !== method){
-            return res.status(405).end();
-        }
-        try {
-            await fn(req , res);
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({error});
-        }
+  method: "GET" | "POST" | "DELETE",
+  fn: (req: NextApiRequest, res: NextApiResponse) => void
+) {
+  return async function (
+    req: NextApiRequest,
+    res: NextApiResponse
+  ): Promise<any> {
+    if (req.method !== method) {
+      return res.status(405).end();
     }
+    try {
+      await fn(req, res);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error });
+    }
+  };
 }
