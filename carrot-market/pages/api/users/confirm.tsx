9dc2b82f76c2c8df85dbd9221eaf04c4188a2c71
hawnbin <dngh0825@naver.com>
2022-09-25 21:30:31 +0900

    fix Serverless Sesstions2

--- carrot-market/pages/api/users/confirm.tsx
+++ carrot-market/pages/api/users/confirm.tsx
@@ -1,26 +1,28 @@
 import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler, {ResponseType} from "@libs/server/withHandler";
-import client from "@libs/server/client"
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
-async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-  console.log(req.session);
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { token } = req.body;
   const exists = await client.token.findUnique({
     where: {
       payload: token,
     },
   });
-  if(!exists) res.status(404).end();
+  if (!exists) res.status(404).end();
   req.session.user = {
     id: exists?.userId,
   };
   await req.session.save();
   res.status(200).end();
-};
+}
 
 export default withIronSessionApiRoute(withHandler("POST", handler), {
-    cookieName: "carrotsession",
-    password: "kou7PZzhCM8eikerfLYyLezGA4AxbFX4",
+  cookieName: "carrotsession",
+  password:
+    "9845904809485098594385093840598df;slkgjfdl;gkfsdjg;ldfksjgdsflgjdfklgjdflgjflkgjdgd",
 });
-
