e1dea196b5bcdffbfb20f77079cfb71753c13b62
hawnbin <dngh0825@naver.com>
2022-09-30 09:40:50 +0900

    Upload API

---
+++ carrot-market/pages/api/products/index.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+// get user profile
+// 모든 api는 실제 백엔드 없이 개별적으로 동작하기 때문에
+// 각 api 마다 type과 withIronSessionApiRoute config를 매번 설정해줘야함
+// 쿠키에 세션이 userId가 저장되어 있기 때문에 Id에 해당하는 user정보를 가져올 수 있음
+
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+//   const { name , price , description } = req.body;
+//   const { user } = req.session; //  === 아래코드
+const {
+    body: { name, price, description},
+    session: {user},
+} = req;
+ const product = await client.product.create({
+        data: {
+            name,
+            price: +price,
+            description,
+            image: "xx",
+            user: {
+                connect: {
+                    id:user?.id,
+                },
+            },
+        },
+  });
+  res.json({
+    ok: true,
+    product ,
+  });
+}
+
+export default withApiSession(
+    withHandler({
+        method: "POST",
+        handler,
+    })
+);
