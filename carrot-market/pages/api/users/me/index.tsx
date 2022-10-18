eaabb1d66c0ff4e031786f4e7f7c50c1597162df
hawnbin <dngh0825@naver.com>
2022-10-18 18:06:27 +0900

    Edit Profile part Two

--- carrot-market/pages/api/users/me/index.tsx
+++ carrot-market/pages/api/users/me/index.tsx
@@ -3,28 +3,100 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
 
-// get user profile
-// 모든 api는 실제 백엔드 없이 개별적으로 동작하기 때문에
-// 각 api 마다 type과 withIronSessionApiRoute config를 매번 설정해줘야함
-// 쿠키에 세션이 userId가 저장되어 있기 때문에 Id에 해당하는 user정보를 가져올 수 있음
-
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const profile = await client.user.findUnique({
-    where : {id:req.session.user?.id},
-});
-  res.json({
-    ok: true,
-    profile,
-  });
+  if (req.method === "GET") {
+    const profile = await client.user.findUnique({
+      where: { id: req.session.user?.id },
+    });
+    res.json({
+      ok: true,
+      profile,
+    });
+  }
+  if (req.method === "POST") {
+    const {
+      session: { user },
+      body: { email, phone, name },
+    } = req;
+    const currentUser = await client.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+    });
+    if (email && email !== currentUser?.email) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            email,
+          },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (alreadyExists) {
+        return res.json({
+          ok: false,
+          error: "Email already taken.",
+        });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          email,
+        },
+      });
+      res.json({ ok: true });
+    }
+    if (phone && phone !== currentUser?.phone) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            phone,
+          },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (alreadyExists) {
+        return res.json({
+          ok: false,
+          error: "Phone already in use.",
+        });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          phone,
+        },
+      });
+      res.json({ ok: true });
+    }
+    if (name) {
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          name,
+        },
+      });
+    }
+    res.json({ ok: true });
+  }
 }
 
 export default withApiSession(
-    withHandler({
-        methods: ["GET"],
-        handler,
-        })
+  withHandler({
+    methods: ["GET", "POST"],
+    handler,
+  })
 );
--- carrot-market/pages/profile/edit.tsx
+++ carrot-market/pages/profile/edit.tsx
@@ -5,25 +5,42 @@ import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
 import { useForm } from "react-hook-form";
 import { useEffect } from "react";
+import useMutation from "@libs/client/useMutation";
 
 interface EditProfileForm {
   email?: string;
   phone?: string;
+  name?:string;
   formErrors?: string;
+}
+
+interface EditProfileResponse {
+  ok:boolean;
+  error?:string;
 }
 
 const EditProfile: NextPage = () => {
   const { user } = useUser();
   const {register , setValue , handleSubmit , setError , formState: {errors}} = useForm<EditProfileForm>();
   useEffect(() => {
+    if(user?.name) setValue("email" , user.name)
     if(user?.email) setValue("email" , user.email)
     if(user?.phone) setValue("phone" , user.phone)
-  },[user , setValue])
-  const onValid = ({email , phone}: EditProfileForm) => {
-    if(email === "" &&  phone === ""){
-        setError("formErrors" , {message: "Email OR Phone number are required."})
+  },[user , setValue]);
+  const [editProfile, {data, loading }] = 
+  useMutation<EditProfileResponse>(`/api/users/me`);
+  const onValid = ({email , phone , name}: EditProfileForm) => {
+    if(loading) return;
+    if(email === "" &&  phone === ""   && name === ""){
+        return setError("formErrors" , {message: "Email OR Phone number are required."});
     }
-  }
+    editProfile({email,phone,name});
+  };
+  useEffect(() => {
+    if(data && !data.ok && data.error) {
+      setError("formErrors", { message: data.error });
+    }
+  }, [data , setError]);
   return (
     <Layout canGoBack title="Edit Profile">
       <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
@@ -43,6 +60,13 @@ const EditProfile: NextPage = () => {
           </label>
         </div>
         <Input 
+          register={register("name")} 
+          required={false}
+          label="Name" 
+          name="name" 
+          type="text" 
+        />
+        <Input 
           register={register("email")} 
           required={false}
           label="Email address" 
@@ -58,7 +82,7 @@ const EditProfile: NextPage = () => {
           kind="phone"
         />
         {errors.formErrors && <span className="my-2 text-red-500 font-medium text-center block">{errors.formErrors.message}</span>}
-        <Button text="Update profile" />
+        <Button text={loading? "Loading..." : "Update profile"} />
       </form>
     </Layout>
   );
