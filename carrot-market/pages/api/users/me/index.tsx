24b6889dab4facf73e923b04753855d7cff83ef2
hawnbin <dngh0825@naver.com>
2022-10-24 11:22:25 +0900

    Serving Images

--- carrot-market/pages/api/users/me/index.tsx
+++ carrot-market/pages/api/users/me/index.tsx
@@ -19,7 +19,7 @@ async function handler(
   if (req.method === "POST") {
     const {
       session: { user },
-      body: { email, phone, name },
+      body: { email, phone, name , avatarId },
     } = req;
     const currentUser = await client.user.findUnique({
       where: {
@@ -90,6 +90,16 @@ async function handler(
         },
       });
     }
+    if(avatarId){
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          avatar: avatarId,
+        },
+      });
+    }
     res.json({ ok: true });
   }
 }
@@ -100,3 +110,4 @@ export default withApiSession(
     handler,
   })
 );
+
