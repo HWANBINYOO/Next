c3f696d5aeff8cb3c9f6295aadb47e6d9f5873d5
hawnbin <dngh0825@naver.com>
2022-10-25 18:27:24 +0900

    Streaming API

--- carrot-market/pages/api/files.ts
+++ carrot-market/pages/api/files.ts
@@ -9,12 +9,12 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const response = await (await fetch(
-    `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ID}/images/v1/direct_upload`,
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ID}/stream/live_inputs`,
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.CF_TOKEN}`,
+        Authorization: `Bearer ${process.env.CF_IMAGES_TOKEN}`,
       },
     }
   )
