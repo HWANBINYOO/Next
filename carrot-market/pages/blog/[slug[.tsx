34894e11a2d1aedf4d5c73a490f6b9c441a37005
hawnbin <dngh0825@naver.com>
2022-10-29 12:26:43 +0900

    getStaticPaths part One

---
+++ carrot-market/pages/blog/[slug[.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from "next"
+
+const Post: NextPage = () => {
+    return (
+        <h1>hi</h1>
+    )
+};
+
+export async function getStaticProps() {
+
+    
+    return {
+      props: {
+      },
+    };
+  }
+
+
+export default Post;
--- carrot-market/pages/blog/index.tsx
+++ carrot-market/pages/blog/index.tsx
@@ -2,11 +2,13 @@ import Layout from "@components/layout";
 import matter from "gray-matter";
 import { readdirSync, readFileSync } from "fs";
 import { NextPage } from "next";
+import Link from "next/link";
 
 interface Post {
     title: string;
     date: string;
     category: string;
+    slug:string;
   }
   
 
@@ -16,12 +18,16 @@ const Blog:NextPage<{posts:Post[]}> = ({posts}) => {
             <h1 className="font-semibold text-center text-xl mt-5 mb-10">Lates Posts:</h1>
             {posts.map((post, index) => (
             <div key={index} className="mb-5">
-                <span className="text-lg text-red-500">{post.title}</span>
-                <div>
+                <Link href={`/blog/${post.slug}`}>
+                  <a href="">
+                  <span className="text-lg text-red-500">{post.title}</span>
+                  <div>
                     <span>
                     {post.date} / {post.category}
                     </span>
-                </div>
+                  </div>
+                  </a>
+                </Link>
             </div>
             ))}
         </Layout>
@@ -31,7 +37,8 @@ const Blog:NextPage<{posts:Post[]}> = ({posts}) => {
 export async function getStaticProps() {
     const blogPosts = readdirSync("./posts").map((file) => {
       const content = readFileSync(`./posts/${file}`, "utf-8");
-      return matter(content).data;
+      const [slug, _] = file.split(".");
+      return {...matter(content).data , slug:file };
     });
     return {
       props: {
