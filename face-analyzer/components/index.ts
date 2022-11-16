e927a3b6a7e93911e605cacf653e232a6d33a7a5
hawnbin <dngh0825@naver.com>
2022-11-16 22:17:56 +0900

    spelling

--- face-analyzer/components/index.ts
+++ face-analyzer/components/index.ts
@@ -1,3 +1,3 @@
 export { default as Header } from './Header'
 export { default as Home } from './Home'
-export { default as Analyzer } from './Analyzer'
+export { default as Analyzer } from './analyzer'
